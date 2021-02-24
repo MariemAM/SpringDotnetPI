013fe3f40391defd971234bbb561286f4708093d
mariem <mariem.aidi@esprit.tn>
2021-02-24 12:29:54 +0100

    First Commit

---
+++ src/main/java/tn/esprit/spring/SpringDotnetPiApplication.java
@@ -0,0 +1,13 @@
+package tn.esprit.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDotnetPiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDotnetPiApplication.class, args);
+	}
+
+}
